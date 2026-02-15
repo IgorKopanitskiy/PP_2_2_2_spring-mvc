81252a1752ee381c07d07eb1650d4824c4ea9129
Igor Kopanitskiy <kopa34kopa@gmail.com>
2026-02-15 21:39:13 +0700

    Solution

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -2,7 +2,7 @@ package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
@@ -11,13 +11,13 @@ import web.service.CarServiceImpl;
 import java.util.List;
 
 @Controller
-public class CarController {
+public class CarsController {
 
     @Autowired
     private CarServiceImpl carService;
 
     @GetMapping(value = "/cars")
-    public String printCars(@RequestParam(value = "count", defaultValue = "5") int count, ModelMap model) {
+    public String printCars(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
         List<Car> carList = carService.getCarSublist(carService.getCarList(),count);
         model.addAttribute("carList", carList);
         return "carsView";
